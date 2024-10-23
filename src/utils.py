d36e83745ce4fdd9a8c550bc6c1027c7b8449cb9
Michael Bumbar <e11775807@student.tuwien.ac.at>
2024-10-23 22:12:25 +0200

    feature: added method to calculate padding in accordance to task description

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
+import math
 import os
 import torch
 import uuid
-
 
 def save_model(config, model,epoch, optimizer, criterion):
     torch.save({
@@ -11,7 +11,6 @@ def save_model(config, model,epoch, optimizer, criterion):
                 'optimizer_state_dict': optimizer.state_dict(),
                 'loss': criterion,
                 }, os.path.join('weights','catCnn'+str(uuid.uuid4())))
-
 
 # a class to control early stopping during the training process to avoid overfitting
 # learned from the article https://machinelearningmastery.com/early-stopping-to-avoid-overtraining-neural-network-models/
@@ -34,3 +33,16 @@ class EarlyStopping:
             self.curr = score
             self.cnt = 0
         return self.stopped
+
+# calcualtes padding given input dim, output dim, kernel size and stride
+# when stride is 1 input and output dimension are identical
+# when stride is 2 output dimension is half of input
+# based on the formula from pytorch docs https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
+def calculate_padding(kernel, stride):
+    if stride == 1:
+        padding = (kernel - 1) // 2
+    elif stride == 2:
+        padding = math.ceil((kernel - 2) / 2)
+    else:
+        raise ValueError("This function only supports stride of 1 or 2.")
+    return padding
